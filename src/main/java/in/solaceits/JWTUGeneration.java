7b2be4bb6fd8a05112d390b6bc09b0947d124d5a
sahudhiraj <dhirajsahu6772@gmail.com>
2022-03-31 08:46:20 +0530

    added remaining 4 methods

--- src/main/java/in/solaceits/JWTUGeneration.java
+++ src/main/java/in/solaceits/JWTUGeneration.java
@@ -3,6 +3,7 @@ package in.solaceits;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.lang.model.element.Modifier;
@@ -18,6 +19,7 @@ import com.squareup.javapoet.TypeSpec;
 import com.sun.jdi.Value;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JWTUGeneration {
 
@@ -44,8 +46,8 @@ public class JWTUGeneration {
 								+ "				.setIssuer(\"SOLACEITS\")\r\n"
 								+ "				.setIssuedAt(new Date(System.currentTimeMillis()))\r\n"
 								+ "				.setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(30)))\r\n"
-								+ "				.signWith(SignatureAlgorithm.HS512, secret)\r\n"
-								+ "				.compact()")
+								+ "				.signWith($L.HS512, secret)\r\n"
+								+ "				.compact()", SignatureAlgorithm.class.getName())
 						.returns(String.class).build()
 						)
 				.addMethod(
@@ -78,7 +80,39 @@ public class JWTUGeneration {
 								)
 						.returns(String.class).build()
 						
-						).build();
+						)
+				.addMethod(
+						MethodSpec.methodBuilder("generateToken")
+						.addModifiers(Modifier.PUBLIC)
+						.addParameter(String.class, "username")
+						.addStatement(
+								"Map<String, Object> claims = new $L<>();\r\n"
+								+ "		return generateToken(claims, username);", HashMap.class.getName()
+								)
+						.returns(String.class).build()
+				        )
+				.addMethod(
+						MethodSpec.methodBuilder("isTokenExpired")
+						.addModifiers(Modifier.PUBLIC)
+						.addParameter(String.class, "token")
+						.addStatement(
+								"final Date expiration = getExpDate(token);\r\n"
+								+ "		return expiration.before(new Date());", Date.class.getName()
+								)
+						.returns(boolean.class).build()
+						)
+				.addMethod(
+						MethodSpec.methodBuilder("isTokenExpired")
+						.addModifiers(Modifier.PUBLIC)
+						.addParameter(String.class, "token")
+						.addParameter(String.class, "username")
+						.addStatement(
+								"String usernameInToken = getUsername(token);\r\n"
+								+ "		return (usernameInToken.equals(username) && !isTokenExpired(token));"
+								)
+						.returns(boolean.class).build()
+						)
+				.build();
 				
 		JavaFile javaFile = JavaFile
 				.builder("in.solaceits",jwtUtil)
